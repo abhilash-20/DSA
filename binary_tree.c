24a4ec1dea9d6a25000ff969706ce48edb7ba06f
Abhilash <abhilashkashyap33@gmail.com>
2023-11-13 23:08:53 +0530

    added the traversals

--- binary_tree.c
+++ binary_tree.c
@@ -1,45 +1,86 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 struct node
 {
     int data;
-    struct node *left,*right;
+    struct node *left, *right;
 };
- struct node* create()
- {
+struct node *create()
+{
     int x;
     struct node *newnode;
-    newnode=(struct node*)malloc(sizeof(struct node));
+    newnode = (struct node *)malloc(sizeof(struct node));
     printf("Enter data(-1 for no node) ");
-    scanf("%d",&x);
-    if(x==-1)
+    scanf("%d", &x);
+    if (x == -1)
     {
         return 0;
     }
-    newnode->data=x;
-    printf("Enter left child of %d\n",x);
-    newnode->left=create();
-    printf("Enter right child of %d\n",x);
-    newnode->right=create();
+    newnode->data = x;
+    printf("Enter left child of %d\n", x);
+    newnode->left = create();
+    printf("Enter right child of %d\n", x);
+    newnode->right = create();
     return newnode;
- }
- void display(struct node *root, int level)
+}
+void display(struct node *root, int level)
 {
     int i;
-    if(root == 0)
+    if (root == 0)
+    {
         return;
+    }
     display(root->right, level + 1);
     printf("\n");
-    for(i = 0; i < level; i++)
+    for (i = 0; i < level; i++)
+    {
         printf("    ");
+    }
     printf("%d\n", root->data);
     display(root->left, level + 1);
 }
+void inorder(struct node *root)
+{
+    if(root==0)
+    {
+        return ;
+    }
+    inorder(root->left);
+    printf("%d ",root->data);
+    inorder(root->right);
+}
+void preorder(struct node *root)
+{
+    if(root==0)
+    {
+        return;
+    }
+    printf("%d ",root->data);
+    preorder(root->left);
+    preorder(root->right);
+}
+
+void postorder(struct node *root)
+{
+    if(root==0)
+    {
+        return;
+    }
+    postorder(root->left);
+    postorder(root->right);
+    printf("%d ",root->data);
+}
 void main()
 {
-    int x;
-    struct node* root;
-    root=0;
-    root=create();
+    struct node *root;
+    root = 0;
+    root = create();
     display(root, 0);
+    printf("\nInorder traversal is: ");
+    inorder(root);
+    printf("\nPreorder traversal is: ");
+    preorder(root);
+    printf("\nPostorder traversal is: ");
+    postorder(root);
+    return ;
 }
